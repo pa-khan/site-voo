5c9fa75219492aa98f661510ac4408ac15da2617
e5ash <io@e5ash.com>
2017-12-28 04:43:47 +0300

    html & text

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,10 @@ gulp.task('browser-sync', function() {
 gulp.task('scripts', function() {
 	return gulp.src([ 
 		'src/template/libs/jquery/dist/jquery.js',
-		'src/template/libs/jquery.maskedinput/dist/jquery.maskedinput.min.js'
+		'src/template/libs/jquery.maskedinput/dist/jquery.maskedinput.min.js',
+		'src/template/libs/fancybox/dist/jquery.fancybox.js',
+		'src/template/libs/jparallax/js/jquery.parallax.js',
+		'src/template/libs/jparallax/js/jquery.event.frame.js'
 		])
 		.pipe(concat('libs.js')) 
 		.pipe(gulp.dest('src/template/scripts')); 
